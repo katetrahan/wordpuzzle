dbee642f447ada5367ea0b775c72f32c6f4b049a
Byron Chang and Kate Trahan <byron@>
2017-12-05 09:38:58 -0800

    add remover, separate front end and back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+var vowelRemover = function(sentence) {
+  var games = sentence.split("");
+  var vowels = ["a", "e", "i", "o", "u"];
+  for (var index = 0; index < games.length; index += 1) {
+    for (var vindex = 0; vindex < vowels.length; vindex += 1) {
+      if (games[index] === vowels[vindex]) {
+        games.splice(index, 1, "-");
+      }
+    }
+  }
+  return games;
+}
+//Front End code
 $(document).ready(function() {
   $("form#sentence").submit(function(event) {
     event.preventDefault();
     var sentence = $("#item1").val();
-    var games = sentence.split("");
-    console.log(games);
-    // debugger;
-    var vowels = ["a", "e", "i", "o", "u"];
-    for (var index = 0; index < games.length; index += 1) {
-      for (var vindex = 0; vindex < vowels.length; vindex += 1) {
-        if (games[index] === vowels[vindex]) {
-          games.splice(index, 1, "-");
-        }
-      }
-    }
-    console.log(games);
+    debugger;
+    var games = vowelRemover(sentence);
+
     $("#sentinput").hide();
     $(".item1").text(games.reduce(reducer));
   });
